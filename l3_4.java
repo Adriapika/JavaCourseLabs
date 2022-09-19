1936d7127af889f941c5efc3c9b4f4f0f97f46b2
Adriapika <73498947+Adriapika@users.noreply.github.com>
2022-09-19 18:59:52 +0300

    Arsenii Driapika, Class Work 2

---
+++ l3_4.java
@@ -0,0 +1,16 @@
+public class l3_4 {
+    public static void main(String[] args) {
+        float temp1 =  (1.f/(10 * 10 * 10 * 10));
+        double temp2 = Math.pow(10, -4);
+        float temp3 = 2.33E5f;
+        double temp4 = 2.33E5;
+        float temp5 = 3.141592653589793f;
+        double temp6 = Math.PI;
+        float temp7  = 2.7182818284590452353602874713527f;
+        double temp8 = Math.E;
+        float temp9 = (float)Math.sqrt(5);
+        double temp10 = Math.sqrt(5);
+        float temp11 = (float)Math.log(100);
+        double temp12 = Math.log(100);
+    }
+}
