cc1120afa291ff7a79e9c4fea815124acbf3ee0f
Adriapika <73498947+Adriapika@users.noreply.github.com>
2022-10-03 17:46:38 +0300

    Driapika Arsenii, Class Work 3

---
+++ l2_1_5.java
@@ -0,0 +1,11 @@
+public class l2_1_5 {
+    float x = 2.0f;
+    String name = "Alexander";
+    public void method(){
+        System.out.print(this.name + this.x);
+    }
+    public static void main(String[] args) {
+        l2_1_5 tmp = new l2_1_5();
+        tmp.method();
+    }
+}
